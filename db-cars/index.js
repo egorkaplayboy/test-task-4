e189a3563e8eccf984830cd63757c160561a38b3
egorkaplayboy <bel04ka228@mail.ru>
2024-03-07 18:26:38 +0300

    feat: made populateTables function

--- db-cars/index.js
+++ db-cars/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { Pool } = require("pg");
-const { createTables } = require("./src/dbSetup");
+const { createTables, populateTables } = require("./src/dbSetup");
 
 const pool = new Pool({
   host: process.env.DB_HOST,
@@ -10,6 +10,16 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
-createTables(pool).catch((err) =>
-  console.error("Error in createTables function:", err)
-);
+const setupDatabase = async () => {
+  await createTables(pool).catch((err) =>
+    console.error("Error in createTables function:", err)
+  );
+
+  await populateTables(pool).catch((err) =>
+    console.error("Error in populateTables function:", err)
+  );
+
+  await pool.end(); // Вызываем pool.end() здесь после завершения всех операций
+};
+
+setupDatabase();
--- db-cars/src/dbSetup.js
+++ db-cars/src/dbSetup.js
@@ -27,10 +27,42 @@ const createTables = async (pool) => {
     console.error("Error creating tables:", err);
   } finally {
     client.release();
-    await pool.end();
+  }
+};
+
+// Присваивание данных к таблицам
+
+const populateTables = async (pool) => {
+  const client = await pool.connect();
+  try {
+    await client.query("INSERT INTO cars (model) VALUES ('Toyota')");
+    await client.query("INSERT INTO cars (model) VALUES ('Honda')");
+    await client.query("INSERT INTO cars (model) VALUES ('Ford')");
+
+    await client.query("INSERT INTO owners (name) VALUES ('John Doe')");
+    await client.query("INSERT INTO owners (name) VALUES ('Jane Smith')");
+
+    const carId1 = 1;
+    const ownerId1 = 1;
+    await client.query(
+      `INSERT INTO ownership (car_id, owner_id, purchase_date) VALUES (${carId1}, ${ownerId1}, '2022-01-01')`
+    );
+
+    const carId2 = 2;
+    const ownerId2 = 2;
+    await client.query(
+      `INSERT INTO ownership (car_id, owner_id, purchase_date) VALUES (${carId2}, ${ownerId2}, '2022-02-15')`
+    );
+
+    console.log("Data inserted into tables successfully.");
+  } catch (err) {
+    console.error("Error inserting data into tables:", err);
+  } finally {
+    client.release();
   }
 };
 
 module.exports = {
   createTables,
+  populateTables,
 };
